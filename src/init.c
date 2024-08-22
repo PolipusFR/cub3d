60e22a9589c6614f558eb39b9404d8d51e8ba916
Louis Sabatier <lsabatie@z2r4p8.42lyon.fr>
2024-08-22 15:07:59 +0200

    fixed out of bounds segfault

--- src/init.c
+++ src/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sben-rho <sben-rho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lsabatie <lsabatie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 16:56:09 by lsabatie          #+#    #+#             */
-/*   Updated: 2024/08/19 15:01:19 by sben-rho         ###   ########.fr       */
+/*   Updated: 2024/08/22 15:01:19 by lsabatie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,41 +61,12 @@ t_game_data	*init_game_data(t_data *data)
 		init_orientation_ns(g_data, data->parse);
 	else
 		init_orientation_ew(g_data, data->parse);
-	g_data->pos_x = data->parse->player[1] + 0.2;
-	g_data->pos_y = data->parse->player[0] + 0.2;
+	g_data->pos_x = data->parse->player[1];
+	g_data->pos_y = data->parse->player[0];
 	g_data->rot_speed = 0.080;
 	g_data->move_speed = 0.070;
 	return (g_data);
 }
-
-// t_parse	*init_parsing_data(void)
-// {
-// 	t_parse	*parse;
-// 	int		i;
-
-// 	i = 0;
-// 	parse = malloc(sizeof(t_parse));
-// 	parse->n = "pics/bluestone.xpm";
-// 	parse->s = "pics/greystone.xpm";
-// 	parse->e = "pics/purplestone.xpm";
-// 	parse->w = "pics/redbrick.xpm";
-// 	parse->color_c = 0x00FFFF;
-// 	parse->color_f = 0x008000;
-// 	parse->player[0] = 12;
-// 	parse->player[1] = 12;
-// 	parse->orientation = 'N';
-// 	parse->map = malloc((MAP_HEIGHT + 2) * sizeof(char *));
-// 	parse->map[0] = ft_strdup("111111111111111111111111");
-// 	i = 1;
-// 	while (i < 24)
-// 	{
-// 		parse->map[i] = ft_strdup("100000000000000000000001");
-// 		i++;
-// 	}
-// 	parse->map[i] = ft_strdup("111111111111111111111111");
-// 	parse->map[i + 1] = 0;
-// 	return (parse);
-// }
 
 t_draw_calc	init_draw(void)
 {
